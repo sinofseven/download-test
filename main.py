85e5973d28084175619a20e54694319552befe3d
sinofseven <em.s.00001@gmail.com>
2025-09-21 16:06:16 +0900

    test v2

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from sentence_transformers import SentenceTransformer
+from datetime import datetime, timezone
 
+dt_before = datetime.now(timezone.utc)
+print(str(dt_before))
 model = SentenceTransformer("multi-qa-mpnet-base-cos-v1")
+dt_after = datetime.now(timezone.utc)
+print(str(dt_after), str(dt_after - dt_before))
+
+print("=" * 20)
+
+dt_before = datetime.now(timezone.utc)
+print(str(dt_before))
+model.encode(["殺して解して並べて揃えて晒してやんよ"], convert_to_tensor=False)
+dt_after = datetime.now(timezone.utc)
+print(str(dt_after), str(dt_after - dt_before))
