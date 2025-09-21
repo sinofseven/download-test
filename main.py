c2dfda6e73962133177e3ccdede1846a7e5afa0d
sinofseven <em.s.00001@gmail.com>
2025-09-21 17:26:31 +0900

    test token

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer
+import tiktoken
 from datetime import datetime, timezone
 
 dt_before = datetime.now(timezone.utc)
@@ -11,10 +12,19 @@ print("=" * 20)
 
 dt_before = datetime.now(timezone.utc)
 print(str(dt_before))
-resp = model.encode(["殺して解して並べて揃えて晒してやんよ"], convert_to_tensor=False)
+resp = tiktoken.encoding_for_model("gpt-4")
 dt_after = datetime.now(timezone.utc)
 print(str(dt_after), str(dt_after - dt_before))
+print(resp)
 
 print("=" * 20)
 
+dt_before = datetime.now(timezone.utc)
+print(str(dt_before))
+resp = model.encode(["殺して解して並べて揃えて晒してやんよ"], convert_to_tensor=False)
+dt_after = datetime.now(timezone.utc)
+print(str(dt_after), str(dt_after - dt_before))
 print(resp)
+
+print("=" * 20)
+
